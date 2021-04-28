85058e450adc470c07670ca6b444f4c074546d8f
fidel bello <59423@bears.smcc.edu>
2021-04-28 00:59:12 -0400

    finished naming table relationships

--- models/Category.js
+++ models/Category.js
@@ -13,6 +13,11 @@ Category.init(
       primaryKey: true,
       autoIncrement: true
   },
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  
   },
   {
     sequelize,
