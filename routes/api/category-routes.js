d43d76cbd6dc627299709e013f3add5c66318e4c
fidel bello <59423@bears.smcc.edu>
2021-04-29 18:18:47 -0400

    updated category by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,6 +52,16 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(categoryData => {
+    if(!categoryData[0]) {
+      res.status(404).json({ message: 'no data found'});
+      return
+    }
+  })
 });
 
 router.delete('/:id', (req, res) => {
