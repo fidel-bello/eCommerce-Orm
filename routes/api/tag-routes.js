108d79522b7193c927a9897cdc3d1d4d0255df65
fidel bello <59423@bears.smcc.edu>
2021-05-01 10:36:48 -0400

    added delete to tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -65,6 +65,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params,id
+    }
+  }).then(tagData => {
+    if(tagData) {
+      res.status(404).json({ message: "no data found"});
+      return;
+    }
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
